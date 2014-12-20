042527d8988eb3486f82d8107f3093d9221a1829
Roscoe A. Bartlett <bartlettra@ornl.gov>
2014-12-20 14:18:12 -0500

    Add behavior for CXXFLAGS/CMAKE_CXX_FLAGS and skip all when USE_XSDK_DEFAULTS=OFF
    Now the XSDKDefaults.cmake module does nothing when USE_XSDK_DEFAULTS=OFF.

--- XSDKDefaults.cmake
+++ XSDKDefaults.cmake
@@ -44,47 +44,54 @@ SET(XSDK_USE_COMPILER_ENV_VARS  OFF  CACHE  BOOL
 PRINT_VAR(USE_XSDK_DEFAULTS)
 PRINT_VAR(XSDK_USE_COMPILER_ENV_VARS)
 
-
-# B) Determine if to set compiler defaults from env var.
-
-SET(GET_COMPILER_DEFAULTS_FROM_ENV  TRUE)
-IF (USE_XSDK_DEFAULTS  AND  NOT  XSDK_USE_COMPILER_ENV_VARS)
-  SET(GET_COMPILER_DEFAULTS_FROM_ENV  FALSE)
-ENDIF()
-
-# C) Set defaults from env vars if asked and otherwise ignore the env vars
-# (which goes against default CMake behavior),
-
-IF (GET_COMPILER_DEFAULTS_FROM_ENV)
-  IF (NOT "$ENV{CXX}" STREQUAL "" AND "${CMAKE_CXX_COMPILER}" STREQUAL "")
-    MESSAGE("Setting CMAKE_CXX_COMPILER from env var CXX='$ENV{CXX}'!")
-    SET(CMAKE_CXX_COMPILER "$ENV{CXX}" CACHE FILEPATH "Set from env var CXX")
-  ENDIF()
-  # ToDo: Handle $ENV{CXXFLAGS} and CMAKE_CXX_FLAGS
-  # ToDo: Handle C and Fortran
-ELSE()
-  IF (NOT "$ENV{CXX}" STREQUAL "" AND "${CMAKE_CXX_COMPILER}" STREQUAL "")
-    MESSAGE("NOT setting CMAKE_CXX_COMPILER from env var CXX='$ENV{CXX}'!")
-    # Got to find the default C++ compiler so ENABLE_LANGAUGE(CXX) does not
-    # pick up the CXX set in the env!  No way to avoid this that I can see.
-    FIND_PROGRAM(DEFAULT_CXX_COMPILER  NAMES  c++  g++)
-    SET(CMAKE_CXX_COMPILER "${DEFAULT_CXX_COMPILER}" CACHE FILEPATH
-      "Ignoring default set by env var CXX")
-  ENDIF()
-  # ToDo: Handle $ENV{CXXFLAGS} and CMAKE_CXX_FLAGS
-  # ToDo: Handle C and Fortran
-ENDIF()
-
-# D) Set defaults for other variables
-
 IF (USE_XSDK_DEFAULTS)
 
+  # B) Determine if to set compiler defaults from env var.
+  
+  SET(GET_COMPILER_DEFAULTS_FROM_ENV  TRUE)
+  IF (USE_XSDK_DEFAULTS  AND  NOT  XSDK_USE_COMPILER_ENV_VARS)
+    SET(GET_COMPILER_DEFAULTS_FROM_ENV  FALSE)
+  ENDIF()
+  
+  # C) Set defaults from env vars if asked and otherwise ignore the env vars
+  # (which goes against default CMake behavior),
+  
+  IF (GET_COMPILER_DEFAULTS_FROM_ENV)
+    IF (NOT "$ENV{CXX}" STREQUAL "" AND "${CMAKE_CXX_COMPILER}" STREQUAL "")
+      MESSAGE("Setting CMAKE_CXX_COMPILER from env var CXX='$ENV{CXX}'!")
+      SET(CMAKE_CXX_COMPILER "$ENV{CXX}" CACHE FILEPATH "Set from env var CXX")
+    ENDIF()
+    IF (NOT "$ENV{CXXFLAGS}" STREQUAL "" AND "${CMAKE_CXX_FLAGS}" STREQUAL "")
+      MESSAGE("Setting CMAKE_CXX_FLAGS from env var CXX='$ENV{CXXFLAGS}'!")
+      SET(CMAKE_CXX_FLAGS "$ENV{CXXFLAGS} " CACHE STRING "Set from env var CXX")
+      # NOTE: We provide a space at the end because that is what raw CMake does!
+    ENDIF()
+    # ToDo: Handle C and Fortran
+  ELSE()
+    IF (NOT "$ENV{CXX}" STREQUAL "" AND "${CMAKE_CXX_COMPILER}" STREQUAL "")
+      MESSAGE("NOT setting CMAKE_CXX_COMPILER from env var CXX='$ENV{CXX}'!")
+      # Got to find the default C++ compiler so ENABLE_LANGAUGE(CXX) does not
+      # pick up the CXX set in the env!  No way to avoid this that I can see.
+      FIND_PROGRAM(DEFAULT_CXX_COMPILER  NAMES  c++  g++)
+      SET(CMAKE_CXX_COMPILER "${DEFAULT_CXX_COMPILER}" CACHE FILEPATH
+        "Ignoring default set by env var CXX")
+    ENDIF()
+    IF (NOT "$ENV{CXXFLAGS}" STREQUAL "" AND "${CMAKE_CXX_FLAGS}" STREQUAL "")
+      MESSAGE("NOT setting CMAKE_CXX_FLAGS from env var CXXFLAGS='$ENV{CXXFLAGS}'!")
+      SET(CMAKE_CXX_FLAGS "" CACHE  STRING
+        "Ignoring default set by env var CXXFLAGS")
+    ENDIF()
+    # ToDo: Handle C and Fortran
+  ENDIF()
+  
+  # D) Set defaults for other variables
+  
   IF ("${BUILD_SHARED_LIBS}"  STREQUAL  "")
     MESSAGE("Setting defalt for BUILD_SHARED_LIBS to TRUE!")
     SET(BUILD_SHARED_LIBS  TRUE  CACHE  BOOL
       "Set by default in XSDK mode")
   ENDIF()
-
+  
   IF ("${CMAKE_BUILD_TYPE}"  STREQUAL  "")
     MESSAGE("Setting defalt for CMAKE_BUILD_TYPE to DEBUG!")
     SET(CMAKE_BUILD_TYPE  DEBUG  CACHE  STRING
